72357069254d5ca640f245c835be600587349bef
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2020-09-27 15:05:48 +0900

    mysql connection success

--- upload/admin.py
+++ upload/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CrawlingData
 
 # Register your models here.
+# 출력할 ResourceAdmin 클래스를 만든다
+class CrawlingDataAdmin(admin.ModelAdmin):
+  list_display = ('id', 'artist','title','h1','s1','v1','h2','s2','v2','h3','s3','v3','imageurl')
+
+admin.site.register(CrawlingData,CrawlingDataAdmin)
+
--- upload/models.py
+++ upload/models.py
@@ -1,39 +1,26 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-from django.conf import settings
-from django.utils import timezone
 
-# Create your models here.
+class CrawlingData(models.Model):
+    artist = models.TextField()
+    title = models.TextField()
+    h1 = models.SmallIntegerField()
+    s1 = models.SmallIntegerField()
+    v1 = models.SmallIntegerField()
+    h2 = models.SmallIntegerField()
+    s2 = models.SmallIntegerField()
+    v2 = models.SmallIntegerField()
+    h3 = models.SmallIntegerField()
+    s3 = models.SmallIntegerField()
+    v3 = models.SmallIntegerField()
+    imageurl = models.TextField()
 
-# #phpmyadmin에서 불러올 data
-# class CrawlingData(models.Model):
-#     artist = models.TextField()
-#     title = models.TextField()
-#     h1 = models.SmallIntegerField()
-#     s1 = models.SmallIntegerField()
-#     v1 = models.SmallIntegerField()
-#     h2 = models.SmallIntegerField()
-#     s2 = models.SmallIntegerField()
-#     v2 = models.SmallIntegerField()
-#     h3 = models.SmallIntegerField()
-#     s3 = models.SmallIntegerField()
-#     v3 = models.SmallIntegerField()
-#     imageurl = models.TextField()
-
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
-
-#사용자 입력 사진 data
-#사용자 입력 댓글 data
+    class Meta:
+        managed = False
+        db_table = 'crawling_data'
